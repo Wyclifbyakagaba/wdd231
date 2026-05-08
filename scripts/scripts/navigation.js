16fb82af4bea282f6149feec5e5b9c53a8465e9b
Wycliff Byakagaba <spearkickboxing20@gmail.com>
2026-05-08 18:58:27 +0600

    scripts/course.js

--- scripts/scripts/navigation.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-const nav = document.getElementById("navMenu");
-
-menuBtn.addEventListener("click", () => {
-  nav.classList.toggle("open");
-});
