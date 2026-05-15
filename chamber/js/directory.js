43756fd086358f5dac8e94defa44d6df94540c01
Wycliff Byakagaba <spearkickboxing20@gmail.com>
2026-05-15 08:13:02 +0600

    chamber/css/large.css chamber/css/normalize.css chamber/css/small.css chamber/data/members.json chamber/directory.html chamber/js/directory.js

---
+++ chamber/js/directory.js
@@ -0,0 +1,43 @@
+const membersContainer = document.querySelector("#members");
+
+async function getMembers() {
+    const response = await fetch("data/members.json");
+    const data = await response.json();
+
+    displayMembers(data);
+}
+
+function displayMembers(members) {
+    members.forEach(member => {
+
+        const card = document.createElement("section");
+
+        card.innerHTML = `
+            <img src="images/${member.image}" alt="${member.name}" loading="lazy">
+            <h3>${member.name}</h3>
+            <p>${member.address}</p>
+            <p>${member.phone}</p>
+            <a href="${member.website}" target="_blank">Visit Website</a>
+            <p>Membership Level: ${member.membership}</p>
+        `;
+
+        membersContainer.appendChild(card);
+    });
+}
+
+getMembers();
+
+document.querySelector("#gridBtn").addEventListener("click", () => {
+    membersContainer.classList.add("grid");
+    membersContainer.classList.remove("list");
+});
+
+document.querySelector("#listBtn").addEventListener("click", () => {
+    membersContainer.classList.add("list");
+    membersContainer.classList.remove("grid");
+});
+
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+`Last Modified: ${document.lastModified}`;
