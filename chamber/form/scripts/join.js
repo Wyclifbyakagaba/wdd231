fabf0595b342fc16cc350b7477df42edfc784d62
Wycliff Byakagaba <spearkickboxing20@gmail.com>
2026-06-04 04:45:27 +0600

    modified:   chamber/form/scripts/join.js

--- chamber/form/scripts/join.js
+++ chamber/form/scripts/join.js
@@ -6,3 +6,28 @@ const navMenu = document.querySelector("#navMenu");
 menuBtn.addEventListener("click", () => {
   navMenu.classList.toggle("open");
 });
+
+// =========================
+// MODALS (ADD THIS PART)
+// =========================
+
+// NP
+document.getElementById("npBtn").addEventListener("click", () => {
+  document.getElementById("npModal").showModal();
+});
+
+// Bronze
+document.getElementById("bronzeBtn").addEventListener("click", () => {
+  document.getElementById("bronzeModal").showModal();
+});
+
+// Silver
+document.getElementById("silverBtn").addEventListener("click", () => {
+  document.getElementById("silverModal").showModal();
+});
+
+// Gold
+document.getElementById("goldBtn").addEventListener("click", () => {
+  document.getElementById("goldModal").showModal();
+});
+
